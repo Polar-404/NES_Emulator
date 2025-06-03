04f668078d90d7d8fc280ac1b50c337d784374d2
LuisMiguel115 <miguelraunh@gmail.com>
2025-06-03 18:41:31 -0300

    primeiro commit

---
+++ target/debug/deps/NES_Emulador-b55a312260d304ee.d
@@ -0,0 +1,5 @@
+c:\Users\migue\OneDrive\Documents\CODEGO\Rust\NES_Emulador\target\debug\deps\libNES_Emulador-b55a312260d304ee.rmeta: src\main.rs
+
+c:\Users\migue\OneDrive\Documents\CODEGO\Rust\NES_Emulador\target\debug\deps\NES_Emulador-b55a312260d304ee.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/NES_Emulador-d1ee807128b791b3.d
@@ -0,0 +1,5 @@
+c:\Users\migue\OneDrive\Documents\CODEGO\Rust\NES_Emulador\target\debug\deps\libNES_Emulador-d1ee807128b791b3.rmeta: src\main.rs
+
+c:\Users\migue\OneDrive\Documents\CODEGO\Rust\NES_Emulador\target\debug\deps\NES_Emulador-d1ee807128b791b3.d: src\main.rs
+
+src\main.rs:
