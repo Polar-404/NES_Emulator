de6b3a9000a22062f0ac995b66a203c5eba8430e
Polaro4 <miguelraunh@gmail.com>
2026-04-24 17:05:29 -0300

    rendering nes frame buffer

--- src/frontend/app.rs
+++ src/frontend/app.rs
@@ -13,9 +13,7 @@ use winit::{
 use crate::{
     engine::instance::EmulatorInstance, 
     frontend::{
-        dock_state::{Tab, NesTabViewer}, 
-        glstate::GLState, 
-        panels::{create_initial_dock_state}
+        dock_state::{NesTabViewer, Tab}, glstate::GLState, nes_texture::NesTexture, panels::create_initial_dock_state
     }
 };
 
@@ -28,7 +26,7 @@ pub struct App {
     dock_state: DockState<Tab>,
 
     nes: Option<EmulatorInstance>,
-    nes_texture: Option<TextureId>,
+    nes_texture: Option<NesTexture>,
 }
 impl App {
     pub fn new() -> Self {
@@ -87,15 +85,32 @@ impl ApplicationHandler for App {
                     gl.clear(glow::COLOR_BUFFER_BIT);
                 }
 
+                if self.nes_texture.is_none() && !self.nes.is_none() {
+                    self.nes_texture = Some(NesTexture::new(gl, egui_glow));
+                }
+
+                if let Some(emu) = &mut self.nes {
+                    emu.run_frame(&mut None);
+                    let texture = self.nes_texture.as_ref().unwrap();
+                    texture.update(gl, emu.frame_buffer());
+                }
+
+                let mut open_rom_requested = false;   
+
                 let dock = &mut self.dock_state;
                 let nes_ref = self.nes.as_ref();
-                let texture = self.nes_texture; 
+                let texture = &self.nes_texture; 
+
+                let texture_opt = self.nes_texture.as_ref().map(|nt| nt.egui_texture_id);
 
                 let repaint_after = egui_glow.run(window, |ctx| { 
                     egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
                         egui::menu::bar(ui, |ui| {
                             ui.menu_button("File", |ui| {
-                                if ui.button("Open ROM...").clicked() { todo!() }
+                                if ui.button("Open ROM...").clicked() {
+                                    open_rom_requested = true;
+                                    ui.close_menu();
+                                }
                                 if ui.button("Exit").clicked() { std::process::exit(0); }
                             });
                             ui.menu_button("NES", |ui| {
@@ -112,10 +127,23 @@ impl ApplicationHandler for App {
                     DockArea::new(dock)
                     .style(Style::from_egui(ctx.style().as_ref()))
                     .show(ctx, &mut NesTabViewer {
-                        nes_texture: texture,
+                        nes_texture: texture_opt,
                         emulator: nes_ref,
                     });
                 });
+
+                if open_rom_requested {
+                    if let Some(path) = crate::frontend::panels::open_rom::open_rom_dialog() {
+                        match crate::engine::instance::EmulatorInstance::new(path) {
+                            Ok(emu) => {
+                                self.nes = Some(emu);
+                            }
+                            Err(e) => {
+                                eprintln!("Falha ao carregar a ROM: {}", e);
+                            }
+                        }
+                    }
+                }
 
                 egui_glow.paint(window);
                 gl_state.swap_buffers();
@@ -125,7 +153,7 @@ impl ApplicationHandler for App {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
-            WindowEvent::KeyboardInput { device_id, event, is_synthetic: true } => {
+            WindowEvent::KeyboardInput { device_id, event, is_synthetic } => {
                 
             }
             _ => { }
--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,5 +1,6 @@
 pub mod app;
 pub mod glstate;
 pub mod dock_state;
+pub mod nes_texture;
 
 pub mod panels;
