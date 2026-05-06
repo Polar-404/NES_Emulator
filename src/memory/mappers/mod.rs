8a09de294ad0b789aceb3d3e2be55039ec6bf855
Polaro4 <miguelraunh@gmail.com>
2026-05-06 15:46:37 -0300

    impl mapper002 logic

--- src/memory/mappers/mod.rs
+++ src/memory/mappers/mod.rs
@@ -1,5 +1,6 @@
 pub mod ines_mapper000;
 pub mod ines_mapper001;
+pub mod ines_mapper002;
 pub mod ines_mapper004;
 pub mod ines_mapper163;
 
@@ -7,5 +8,6 @@ pub mod dummy_mapper;
 
 pub use self::ines_mapper000::InesMapper000;
 pub use self::ines_mapper001::InesMapper001;
+pub use self::ines_mapper002::InesMapper002;
 pub use self::ines_mapper004::InesMapper004;
 pub use self::ines_mapper163::InesMapper163;
